c4aa2f20907ecb64348c1afecf40e7ae427e5494
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-11-25 09:56:11 +0800

    docker 1.9 upgrade, parameterisation, remove onbuild hacks

--- build.sh
+++ build.sh
@@ -9,6 +9,13 @@ set -e
 REPO="muccg"
 DATE=`date +%Y.%m.%d`
 
+DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+HTTP_PROXY="http://${DOCKER_HOST}:3128"
+PIP_INDEX_URL="http://${DOCKER_HOST}:3141/root/pypi/+simple/"
+PIP_TRUSTED_HOST=${DOCKER_HOST}
+
+: ${DOCKER_BUILD_OPTIONS:="--pull=true --build-arg PIP_TRUSTED_HOST=${PIP_TRUSTED_HOST} --build-arg PIP_INDEX_URL=${PIP_INDEX_URL} --build-arg HTTP_PROXY=${HTTP_PROXY} --build-arg http_proxy=${HTTP_PROXY}"}
+
 # build dirs, top level is python version
 for pythondir in */
 do
@@ -25,14 +32,15 @@ do
             distrover=`basename ${distroverdir}`
 
             image="${REPO}/python-base:${distro}${distrover}-${pythonver}"
-            #echo "################################################################### ${image}"
+            echo "################################################################### ${image}"
         
             ## warm up cache for CI
             docker pull ${image} || true
 
             ## build
-            docker build --pull=true -t ${image}-${DATE} ${distroverdir}
-            docker build -t ${image} ${distroverdir}
+            set -x
+            docker build ${DOCKER_BUILD_OPTIONS} -t ${image}-${DATE} ${distroverdir}
+            docker build ${DOCKER_BUILD_OPTIONS} -t ${image} ${distroverdir}
 
             ## for logging in CI
             docker inspect ${image}-${DATE}
@@ -40,7 +48,7 @@ do
             # push
             docker push ${image}-${DATE}
             docker push ${image}
-
+            set +x
         done
     done
 done
